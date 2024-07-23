b8d8f788daeb56f2f974ad0df5a159caddc1d62c
Lokesh Kumar <Lokesh.Kumar@philips.com>
2024-07-23 15:18:11 +0530

    Gtest passed

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,45 @@
 #include "stats.h"
+#include <numeric>
+#include <algorithm>
+#include <iostream>
+using namespace Statistics;
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+double findAverage(const std::vector<double> &data)
+{
+    double sum = std::accumulate(data.begin(),data.end(),0.0);
+    auto n = data.size();
+    double avg = sum/n;
+    return avg;
 }
+
+double findMax(const std::vector<double> &data)
+{
+    double mx = *std::max_element(data.begin(),data.end());
+    return mx;
+}
+
+double findMin(const std::vector<double> &data)
+{
+    double mn = *std::min_element(data.begin(),data.end());
+    return mn;
+}
+
+Stats Statistics::ComputeStatistics(const std::vector<double>& data) {
+    //Implement statistics here
+    
+    double avg=NAN, mx=NAN, mn=NAN;
+    auto n = data.size();
+    if(n)
+    {
+        avg = findAverage(data);
+        mx = findMax(data);
+        mn = findMin(data);
+    }
+
+    Stats obj;
+    obj.average = avg;
+    obj.max = mx;
+    obj.min = mn;
+    return obj;
+}
+
